e17f97902eb6518d6dde0693a519b53f112ab874
ismail095 <ismail.hassab01@gmail.com>
2021-05-25 19:25:28 -0400

    shared on github

--- src/test/java/BranchingPractice.java
+++ src/test/java/BranchingPractice.java
@@ -2,5 +2,11 @@ public class BranchingPractice {
 
     public static void main(String[] args) {
 
+        // created new project
+        // enabled version control
+        // added .gitignore with .idea, target folder and .iml file
+        //made initial commit by keeping track of all foles
+        // shared project on github
+
     }
 }
